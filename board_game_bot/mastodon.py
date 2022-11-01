5355dc88abf481c98a68dec74b3082396d4fd68a
Markus Schepke <markus.schepke@wolt.com>
2022-11-01 22:19:30 +0200

    added draft of mastodon.py

---
+++ board_game_bot/mastodon.py
@@ -0,0 +1,20 @@
+import html2text
+from mastodon import Mastodon, StreamListener
+
+h = html2text.HTML2Text()
+h.ignore_emphasis = True
+h.ignore_links = True
+h.body_width = None
+
+
+class MyStreamListener(StreamListener):
+    def on_update(self, status):
+        print(type(status))
+        print(status)
+        print(status["content"])
+        print(h.handle(status["content"]))
+
+
+m = Mastodon(api_base_url="https://mastodon.social")
+m.verify_minimum_version("1.1.0")
+m.stream_hashtag("introduction", MyStreamListener())
